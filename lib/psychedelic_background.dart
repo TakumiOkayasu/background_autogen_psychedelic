cb78dc5ba24fd6291444d8bf8a8357775887c45b
TakumiOkayasu <takumi_okys@icloud.com>
2026-03-09 12:38:02 +0900

    refactor: DebugOverlayとControlPanelの責務分離

--- lib/psychedelic_background.dart
+++ lib/psychedelic_background.dart
@@ -1,6 +1,6 @@
 export 'interface/shader_config.dart';
 export 'manager/background_manager.dart';
 export 'provider/shader_provider.dart';
-export 'widget/color_overlay_widget.dart';
+export 'widget/control_panel_widget.dart';
 export 'widget/debug_overlay_widget.dart';
 export 'widget/psychedelic_background_widget.dart';
